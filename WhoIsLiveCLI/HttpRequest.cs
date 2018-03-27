258be9d06515372b3894b12cb65194f5aa90b131
danis <danisen98@gmail.com>
2018-03-28 00:14:05 +0300

    Add project files.

---
+++ WhoIsLiveCLI/HttpRequest.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Flurl.Http;
+
+namespace WhoIsLiveCLI
+{
+    class HttpRequest
+    {
+        private string oauth = "b4ju86p4tb4nv6hn2ubbdsqox6iczd";
+         
+        public async Task<dynamic> GetData(string channel, string pagination)
+        {
+            string requestString = "https://api.twitch.tv/helix/users/follows?from_id=" + channel+ "&first=100&after=" +pagination;
+            var responseString = await requestString.WithHeader("Client-ID", oauth).GetJsonAsync();
+            return responseString;
+        }
+        public async Task<dynamic> GetUserData(string channel)
+        {
+            string requestString = "https://api.twitch.tv/helix/streams?user_id=" + channel;
+            var responseString = await requestString.WithHeader("Client-ID", oauth).GetJsonAsync();
+            return responseString;
+        }
+        public async Task<dynamic> GetUserName(string channel)
+        {
+            string requestString = "https://api.twitch.tv/helix/users/?&id=" + channel;
+            var responseString = await requestString.WithHeader("Client-ID", oauth).GetJsonAsync();
+            return responseString;
+        }
+    }
+}
---
+++ WhoIsLiveCLI/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WhoIsLiveCLI
+{
+    static class Program
+    {
+        static void Main()
+        {
+            string channel = "189889981";
+            var request = new HttpRequest();
+            dynamic response = request.GetData(channel, "").Result;
+            List<string> channelsList = new List<string>();
+            List<List<string>> channelsListFull = new List<List<string>>();
+            for (int i = 0; i < Convert.ToInt32(response.total); i++)
+            {
+                channelsList.Add(response.data[i % 100].to_id);
+                if (i % 100 == 99)
+                {
+                    response = request.GetData(channel, response.pagination.cursor).Result;
+                    channelsList = new List<string>();
+                }
+            }
+
+            if (channelsList.Count != 0)
+            {
+                channelsListFull.Add(channelsList);
+            }
+
+            var liveListFull = new List<List<string>>();
+            bool fl = false;
+            foreach (List<string> item in channelsListFull)
+            {
+
+                var LiveRequest = String.Join("&user_id=", item.ToArray());
+                dynamic responseChannel = request.GetUserData(LiveRequest).Result;
+                var liveList = new List<List<string>>();
+
+                for (int i = 0; i < item.Count; i++)
+                {
+                    try
+                    {
+                        var tempList = new List<string>(){ responseChannel.data[i].user_id, responseChannel.data[i].game_id, responseChannel.data[i].viewer_count.ToString(), responseChannel.data[i].title };
+                       
+                        liveList.Add(tempList);
+                    }
+                    catch (ArgumentOutOfRangeException)
+                    {
+                        fl = true;
+                        break;
+                    }
+                }
+
+                liveListFull.AddRange(liveList);
+                if (fl)
+                {
+                    break;
+                }
+            }
+
+            List<string> ListNameRequest = new List<string>();
+            for (var i = 0; i < liveListFull.Count; i++)
+            {
+                ListNameRequest.Add(liveListFull[i][0]);
+                if ((i % 100 == 99) || (i == liveListFull.Count - 1))
+                {
+                    string NameRequest = String.Join("&id=", ListNameRequest.ToArray());
+                    dynamic responseLive = request.GetUserName(NameRequest).Result;
+                    for (int j = 0; j <= i; j++)
+                    {
+                        Console.WriteLine(String.Format("{0, 25} {1, 7} {2, 30} ", responseLive.data[j].login, liveListFull[j][2], liveListFull[j][3]));
+                    }
+                }
+            }
+        }
+    }
+}
+
+
